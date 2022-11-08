9b3e32f7716f55a3605086d17a422010fa4b0ad0
dobrosav13 <tufegdzic.ra112.2019@uns.ac.rs>
2022-11-09 00:53:07 +0100

    Update User.cs

--- ISA-Backend/src/BloodBankLibrary/Core/Model/User.cs
+++ ISA-Backend/src/BloodBankLibrary/Core/Model/User.cs
@@ -20,7 +20,79 @@ namespace BloodBankLibrary.Core.Model
         Gender gender;
         string profession;
         string workplace;
+        public class UserBuilder
+        {
+            public UserType userType;
+            public int id;
+            public string email;
+            public string password;
+            public string name;
+            public string adress;
+            public string phoneNumber;
+            public string jmbg;
+            public Gender gender;
+            public string profession;
+            public string workplace;
+            public int idOfCenter;
 
+            public UserBuilder IDOfCenter(int idOfCenter)
+            {
+                this.idOfCenter = idOfCenter;
+                return this;
+            }
+
+            public UserBuilder Workplace(string workplace)
+            {
+                this.workplace = workplace;
+                return this;
+            }
+
+            public UserBuilder Profession(string profession)
+            {
+                this.profession = profession;
+                return this;
+            }
+
+            public UserBuilder Gender(Gender gender)
+            {
+                this.gender = gender;
+                return this;
+            }
+
+            public UserBuilder JMBG(string jmbg)
+            {
+                this.jmbg = jmbg;
+                return this;
+            }
+
+            public UserBuilder PhoneNumber(string phoneNumber)
+            {
+                this.phoneNumber = phoneNumber;
+                return this;
+            }
+
+            public UserBuilder Adress(string adress)
+            {
+                this.adress = adress;
+                return this;
+            }
+
+            public UserBuilder UserTemplate(UserType userType, int id, string email, string password, string name)
+            {
+                this.userType = userType;
+                this.id = id;
+                this.email = email;
+                this.password = password;
+                this.name = name;
+                return this;
+            }
+
+            public User build()
+            {
+                User user = new User(this);
+                return user;
+            }
+        }
         public User(UserType userType, int idOfCenter, int id, string email,
             string password, string name, string adress, string phoneNumber, 
             string jmbg, Gender gender, string profession, string workplace)
@@ -37,6 +109,21 @@ namespace BloodBankLibrary.Core.Model
             this.gender = gender;
             this.profession = profession;
             this.workplace = workplace;
+        }
+        public User(UserBuilder builder)
+        {
+            this.userType = builder.userType;
+            this.idOfCenter = builder.idOfCenter;
+            this.id = builder.id;
+            this.email = builder.email;
+            this.password = builder.password;
+            this.name = builder.name;
+            this.adress = builder.adress;
+            this.phoneNumber = builder.phoneNumber;
+            this.jmbg = builder.jmbg;
+            this.gender = builder.gender;
+            this.profession = builder.profession;
+            this.workplace = builder.workplace;
         }
 
         public int IdOfCenter { get => idOfCenter; set => idOfCenter = value; }
