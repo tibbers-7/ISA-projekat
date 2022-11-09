68ac032b81124f735c24f42b15bf3426ea5ce127
tibbers-7 <dmitrovic.ra41.2019@uns.ac.rs>
2022-11-09 15:56:47 +0100

    Revert "gyugyjg"
    This reverts commit 4aff3dbfba78f49ad53748ec05fe3be1fbd3fb4d.

--- ISA-Frontend/src/app/modules/blood-bank/blood-bank.module.ts
+++ ISA-Frontend/src/app/modules/blood-bank/blood-bank.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
 import { MaterialModule } from "src/app/material/material.module";
-import { RegistrationComponent } from './registration/registration/registration.component';
+import { CreateRoomComponent } from "./create-room/create-room.component";
+import { RoomDetailComponent } from "./room-detail/room-detail.component";
+import { RoomsComponent } from "./rooms/rooms.component";
+import { UpdateRoomComponent } from "./update-room/update-room.component";
 
 const routes: Routes = [
+  { path: 'rooms', component: RoomsComponent },
+  { path: 'rooms/add', component: CreateRoomComponent },
+  { path: 'rooms/:id', component: RoomDetailComponent },  
+  { path: 'rooms/:id/update', component: UpdateRoomComponent },
 ];
 
 @NgModule({
   declarations: [
-    RegistrationComponent
+    RoomsComponent,
+    RoomDetailComponent,
+    CreateRoomComponent,
+    UpdateRoomComponent
   ],
   imports: [
     CommonModule,
