7918bb03118226b4bead5c01c8a36bdabbb0220b
DanaBrasanac <danabrasanac@gmail.com>
2022-11-09 16:02:19 +0100

    fixfixif

--- ISA-Frontend/src/app/modules/blood-bank/blood-bank.module.ts
+++ ISA-Frontend/src/app/modules/blood-bank/blood-bank.module.ts
@@ -3,24 +3,17 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { RouterModule, Routes } from "@angular/router";
 import { MaterialModule } from "src/app/material/material.module";
-import { CreateRoomComponent } from "./create-room/create-room.component";
-import { RoomDetailComponent } from "./room-detail/room-detail.component";
-import { RoomsComponent } from "./rooms/rooms.component";
-import { UpdateRoomComponent } from "./update-room/update-room.component";
+import { BloodCenterProfileComponent } from "./blood-center-profile/blood-center-profile.component";
+
 
 const routes: Routes = [
-  { path: 'rooms', component: RoomsComponent },
-  { path: 'rooms/add', component: CreateRoomComponent },
-  { path: 'rooms/:id', component: RoomDetailComponent },  
-  { path: 'rooms/:id/update', component: UpdateRoomComponent },
+  { path: 'blood-centers/:id', component: BloodCenterProfileComponent}
+  
 ];
 
 @NgModule({
   declarations: [
-    RoomsComponent,
-    RoomDetailComponent,
-    CreateRoomComponent,
-    UpdateRoomComponent
+    BloodCenterProfileComponent
   ],
   imports: [
     CommonModule,
