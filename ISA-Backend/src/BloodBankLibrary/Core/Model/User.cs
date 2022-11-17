5217e115bc7d7b7c46d55e7884474bfaec246055
tibbers-7 <dmitrovic.ra41.2019@uns.ac.rs>
2022-11-17 08:37:31 +0100

    Update User.cs

--- ISA-Backend/src/BloodBankLibrary/Core/Model/User.cs
+++ ISA-Backend/src/BloodBankLibrary/Core/Model/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,11 @@ namespace BloodBankLibrary.Core.Model
 {
     public class User
     {
-
+        [Key]
+        int id;
         UserType userType;
         int idOfCenter;
-        int id;
+        
         string email;
         string password;
         string name;
