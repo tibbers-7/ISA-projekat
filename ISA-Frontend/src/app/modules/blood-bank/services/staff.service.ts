a2c691f5c911b91ac7a9140b37818e022b7c343a
DanaBrasanac <93882241+DanaBrasanac@users.noreply.github.com>
2022-12-18 03:53:11 +0100

    something

---
+++ ISA-Frontend/src/app/modules/blood-bank/services/staff.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Staff } from '../model/staff.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StaffService {
+
+  apiHost: string = 'http://localhost:16177/';
+  headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(private http: HttpClient) { }
+
+  getStaff(id: number): Observable<Staff> {
+    return this.http.get<Staff>(this.apiHost + 'api/Staff/' + id, { headers: this.headers });
+  }
+
+  updateStaff(staff: any): Observable<any> {
+    return this.http.put<any>(this.apiHost + 'api/Staff/' + staff.id, staff, { headers: this.headers });
+  }
+}
